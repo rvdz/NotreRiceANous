0f800aaffb75bd22242f21fb938bc30cc8e2e498
Yann Colina <yanncolina@gmail.com>
2018-05-06 15:49:35 +0200

    [ubuntu] Added install dependencies support with Docker

---
+++ configure.sh
@@ -0,0 +1,80 @@
+#/bin/bash
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: ./instal.sh --wallpaper <path/to/wallpaper> --yabar <config-name>"
+    echo "Available yabar configs: 'yabarobin', 'yabaromain' and 'yannbar'"
+fi
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -w|--wallpaper)
+    WALLPAPER_PATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -y|--yabar)
+    YABAR_CONFIG_NAME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+I3_CONFIG=~/.config/i3/config
+YABAR_DIR=~/.config/yabar
+
+i3 () {
+    mkdir -p ~/.config/i3
+    echo "Setting i3"
+    cp ./i3/config $I3_CONFIG
+}
+
+wallpaper () {
+    # Escaping '/'
+    wallpaper_path=python3 -c "print('$WALLPAPER_PATH'.replace('/', '\/'))"
+    sed -i "" "s/WALLPAPER_PATH/$WALLPAPER_PATH/g" $I3_CONFIG
+}
+
+
+yabar () {
+    echo "Setting Yabar"
+    mkdir -p ~/.config
+    cp -r yabar  ~/.config
+
+    # Escaping '/'
+    yabar_dir=$(python3 -c "print('$YABAR_DIR'.replace('/', '\/'))")
+    sed -i "" "s/YABAR_CONFIG_FILE/$yabar_dir\/$YABAR_CONFIG_NAME/g" $I3_CONFIG
+}
+
+rofi () {
+    echo "Setting Rofi"
+    echo "NOT IMPLEMENTED"
+}
+
+vim () {
+    echo "Setting Vim"
+    ./vim/install_plugins.sh
+    cp ./vim/vimrc ~/.vimrc
+}
+
+compton () {
+    mkdir -p ~/.config
+    cp ./compton/compton.conf ~/.config
+    echo "exec compton  --config ~/.config/compton.conf -b" >> $I3_CONFIG
+}
+
+i3
+wallpaper
+yabar
+rofi
+vim
+compton
---
+++ install_xenial.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+# Ubuntu Xenial and Debian Strech
+
+essentials () {
+    apt update
+    apt install -y git python-pip python3-pip curl build-essential feh compton wget
+}
+
+i3 () {
+    /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb keyring.deb SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
+    dpkg -i ./keyring.deb
+    echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+
+    apt update
+    apt install -y i3
+}
+
+i3_gaps () {
+    apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
+                libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \
+                libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+                libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf
+
+    # xcb-util-xrm
+    wget https://github.com/Airblader/xcb-util-xrm/releases/download/v1.3/xcb-util-xrm-1.3.tar.gz
+    tar -zxf xcb-util-xrm-1.3.tar.gz
+    cd ./xcb-util-xrm-1.3
+    ./configure
+    make
+    make install
+    cd ../
+
+    # i3-gaps
+    git clone https://www.github.com/Airblader/i3 i3-gaps
+    cd ./i3-gaps
+    autoreconf --force --install
+    rm -rf build/
+    mkdir -p build
+    cd build
+    ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+
+    make
+    make install
+    cd ../../
+}
+
+yabar () {
+    apt install -y libcairo2-dev libpango1.0-dev libconfig-dev libxcb-randr0-dev \
+                    libxcb-ewmh-dev libxcb-icccm4-dev libgdk-pixbuf2.0-dev libasound2-dev \
+                    libiw-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb-xkb-dev
+    git clone https://github.com/geommer/yabar.git
+    cd ./yabar
+
+    make yabar
+    # Remove doc creation (requires latex)
+    sed -i "/\b\(MANPREFIX\)\b/d" Makefile
+    make install
+    cd ../
+}
+
+rofi () {
+    apt install -y autoconf automake flex xcb libpango1.0-dev \
+                libpangocairo-1.0-0 librsvg2-dev libstartup-notification0-dev \
+                libxkbcommon-dev libxkbcommon-x11-dev
+
+    # Bison parser
+    wget http://mirror.ibcp.fr/pub/gnu/bison/bison-1.25.tar.gz
+    tar -xvf bison-1.25.tar.gz
+    cd ./bison-1.25
+    ./configure
+    make
+    make install
+    cd ../
+
+    # Check >= 0.11
+    wget https://github.com/libcheck/check/releases/download/0.12.0/check-0.12.0.tar.gz
+    tar -zxf check-0.12.0.tar.gz
+    cd ./check-0.12.0
+    ./configure
+    make
+    make install
+    cd ../
+
+    # Rofi
+    wget https://github.com/DaveDavenport/rofi/releases/download/1.5.1/rofi-1.5.1.tar.gz
+    tar -xvf rofi-1.5.1.tar.gz
+    cd ./rofi-1.5.1
+    ./configure
+    make
+    make install
+    cd ../
+}
+
+yabar_utils () {
+    apt install -y amixer
+    pip install i3ipc
+}
+
+essentials
+i3
+i3_gaps
+yabar
+rofi
+yabar_utils
