88668848c9a1a77c7cfa17781f12e872f5f3f089
Yann Colina <yann.colina@gmail.com>
2018-12-02 15:38:51 +0100

    [misc]
    - Updated keyboard config
    - Improved getaudiotrack

--- yabar/scripts/getaudiotrack.py
+++ yabar/scripts/getaudiotrack.py
@@ -22,6 +22,16 @@ def is_playing():
     return status == "Playing"
 
 
+def is_paused():
+    status = sp.check_output(["playerctl", "status"])[:-1].decode("utf-8")
+    return status == "Paused"
+
+
+def is_active():
+    status = sp.check_output(["playerctl", "status"])[:-1].decode("utf-8")
+    return status != "No players found"
+
+
 def print_string(msg):
     sys.stdout.write(msg)
     sys.stdout.flush()
@@ -29,14 +39,20 @@ def print_string(msg):
 
 def on_track_change(self, e):
     # Wait for the player to start/stop before checking its new status
-    time.sleep(0.1)
-    if is_playing():
-        print_string("{} - {}".format(get_artist(), get_title()))
-    else:
+    time.sleep(0.5)
+    if not is_active():
         print("")
         sys.stdout.flush()
+        return
+
+    msg = "{} - {}".format(get_artist(), get_title())
+    if is_playing():
+        print_string(" {}".format(msg))
+    elif is_paused():
+        print_string(" {}".format(msg))
+    return
 
 
 i3 = i3ipc.Connection()
-i3.on("binding", on_track_change)
+i3.on("window", on_track_change)
 i3.main()
